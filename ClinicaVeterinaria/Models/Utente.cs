910c8b1945dc114faaf33ab40f1510d7f757ba87
Annunziata Antonio Massimo <ant.annunz.02@gmail.com>
2024-08-05 11:32:50 +0200

    Classes

---
+++ ClinicaVeterinaria/Models/Utente.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClinicaVeterinaria.Models
+{
+
+    public class Utente
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required, StringLength(20)]
+        public string Username { get; set; }
+
+        [Required, StringLength(20)]
+        public string Password { get; set; }
+
+        [Required]
+        public string Ruolo { get; set; } = "User";
+    }
+
+}
